8f301a834488efcb4eeb81a3c2dfec58203d45cf
Dmitri Laptev <dmitri.laptev@sidecaps.com>
2023-01-11 00:18:02 +0100

    copy generated command to clipboard

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,51 @@
 <script setup>
 import { ref } from "vue";
-const textarea =
-  ref(`https://playlist.servicecdn.ru/player/b49e76f15b90f375c7d7710decc213cb/00d6572b09b386d90dcb1ae2baf20300/master.m3u8?user-cdn=gcore&acc-id=0&user-id=136457026&loc-mode=ru&version=5%3A2%3A1%3A0%3A2%3Agcore&consumer=vod&jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyLWlkIjoxMzY0NTcwMjYsImNvbnN1bWVyIjoidm9kIn0.hiFt-yf8Ut3WmV9jbfb2IHucqTsCwVff7jFvCDLdYQk&gzipoff=1
-https://player02.getcourse.ru/player/dead81caf4d477a62370c07e793c6f47/d3e5ee7e06934bcb61b93177c4456bbf/master.m3u8?user-cdn=gcore&acc-id=0&user-id=136457026&loc-mode=ru&version=5%3A0%3A1%3A0%3A0%3Agcore&consumer=vod&jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyLWlkIjoxMzY0NTcwMjYsImNvbnN1bWVyIjoidm9kIn0.hiFt-yf8Ut3WmV9jbfb2IHucqTsCwVff7jFvCDLdYQk
-https://playlist.servicecdn.ru/player/5ed802a53e54d860ece76ce6e600da94/c78bc57bee63f92d1508c2ada7779b85/master.m3u8?user-cdn=gcore&acc-id=0&user-id=136457026&loc-mode=ru&version=5%3A2%3A1%3A0%3A2%3Agcore&consumer=vod&jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyLWlkIjoxMzY0NTcwMjYsImNvbnN1bWVyIjoidm9kIn0.hiFt-yf8Ut3WmV9jbfb2IHucqTsCwVff7jFvCDLdYQk&gzipoff=1`);
+const textarea = ref("")
 const error = ref("");
 const separator = ref("\n");
+const lession = ref("");
+const success = ref("");
 const separatorList = [
   { name: "Enter", value: "\n" },
   { name: "Comma", value: "," },
   { name: "Semicolon", value: ";" },
 ];
 function generate() {
-  setTimeout(() => {
-    error.value = "";
-  }, 2000);
+  if (error.value) {
+    setTimeout(() => {
+      error.value = "";
+    }, 2000);
+  }
   if (!textarea.value) {
-    error.value = "textarea is empty";
+    error.value = "Textarea is empty";
   }
   if (!textarea.value.includes("http") && !textarea.value.includes(".m3u8")) {
-    error.value = "no .m3u8 link found";
+    error.value = "No .m3u8 link found";
   }
   const links = textarea.value.split(separator.value);
   console.log(links);
-}
-function removeNotification() {
-  error.value = "";
-}
-function changeSeparator(value) {
-  separator.value = value;
+  let result = "";
+  for (let i = 0; i < links.length; i++) {
+    if (links[i]) {
+      result += `ffmpeg -i "${links[i]}" -c copy -bsf:a aac_adtstoasc "${
+        lession.value
+      }-${i + 1}.mp4";`;
+    }
+  }
+  console.log(result);
+  if (result) {
+    const el = document.createElement("textarea");
+    el.value = result;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
+    success.value =
+      "Done! Commands have been copied to the clipboard. Please open your Terminal and paste your generated commands";
+    setTimeout(() => {
+      success.value = "";
+    }, 3000);
+  }
 }
 </script>
 
@@ -41,10 +58,20 @@ function changeSeparator(value) {
           class="notification is-danger"
         >
           <button
-            @click="removeNotification()"
+            @click="error = ''"
             class="delete"
           ></button>
           {{ error }}
+        </div>
+        <div
+          v-if="success"
+          class="notification is-info"
+        >
+          <button
+            @click="success = ''"
+            class="delete"
+          ></button>
+          {{ success }}
         </div>
         <p class="title">.m3u8 Download Helper</p>
         <p class="subtitle">Paste Links here ⬇️</p>
@@ -53,8 +80,8 @@ function changeSeparator(value) {
           v-model="textarea"
           placeholder="Link with .m3u8 File"
         ></textarea>
-        <div class="has-text-left mt-4">
-          <div class="field">
+        <div class="has-text-left mt-4 columns">
+          <div class="field column">
             <label class="label">Separator</label>
             <div class="control">
               <div class="select">
@@ -68,6 +95,18 @@ function changeSeparator(value) {
                   </option>
                 </select>
               </div>
+            </div>
+          </div>
+
+          <div class="field column">
+            <label class="label">Lession name</label>
+            <div class="control">
+              <input
+                class="input"
+                type="text"
+                v-model="lession"
+                placeholder="Lession name"
+              />
             </div>
           </div>
         </div>
